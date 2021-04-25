bf30d0d97e9fa1de63a35105c152cd582537fa98
Omar Hakem <youtarawneh997@gmail.com>
2021-04-25 03:00:58 +0300

    adding the login page for admin

--- src/admin/router/router.js
+++ src/admin/router/router.js
@@ -5,18 +5,18 @@ const AdminBroExpressjs = require('@admin-bro/express');
 const Admin = require('../model/admin.js');
 const bcrypt = require('bcrypt');
 
-// const router = AdminBroExpressjs.buildAuthenticatedRouter(adminBro, {
-//   authenticate: async (email, password) => {
-//     const user = await Admin.findOne({ email });
-//     if (user) {
-//       const matched = await bcrypt.compare(password, user.encryptedPassword);
-//       if (matched) {
-//         return user;
-//       }
-//     }
-//     return false;
-//   },
-//   cookiePassword: 'smile-squad-alpha-version',
-// });
-const router = AdminBroExpressjs.buildRouter(adminBro);
+const router = AdminBroExpressjs.buildAuthenticatedRouter(adminBro, {
+  authenticate: async (email, password) => {
+    const user = await Admin.findOne({ email });
+    if (user) {
+      const matched = await bcrypt.compare(password, user.encryptedPassword);
+      if (matched) {
+        return user;
+      }
+    }
+    return false;
+  },
+  cookiePassword: 'smile-squad-alpha-version',
+});
+// const router = AdminBroExpressjs.buildRouter(adminBro);
 module.exports = router;
